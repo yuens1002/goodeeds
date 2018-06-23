57e2b8fd4ac7e9127246bdda92d89d307789548e
sunny yuen <yuens1002@gmail.com>
2018-06-23 09:53:23 -0400

    na

--- src/api/firebaseConfig.js
+++ src/api/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
 // Initialize Firebase
-  apiKey: 'AIzaSyACY8gEVUU6_MjhFawI19LDbJyR3c_LsC8',
-  authDomain: 'vue-firebase-f11bb.firebaseapp.com',
-  databaseURL: 'https://vue-firebase-f11bb.firebaseio.com',
-  projectId: 'vue-firebase-f11bb',
-  storageBucket: 'vue-firebase-f11bb.appspot.com',
-  messagingSenderId: '395185527573'
+  apiKey: 'your_api_Key',
+  authDomain: 'your_auth_Domain',
+  databaseURL: 'your_database_URL',
+  projectId: 'your_project_Id',
+  storageBucket: 'your_storage_Bucket',
+  messagingSenderId: '_your_messageing_Sender_id'
 }
